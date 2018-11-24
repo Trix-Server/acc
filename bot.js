17a3e4f4a855e3f73ec77c55559adc1ccfec90f1
ahmad107996 <36535354+ahmad107996@users.noreply.github.com>
2018-11-24 18:48:30 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -11,5 +11,5 @@ console.log(`لا تنسا الاششتراك `)
 
 
 
-client.login(process.env.TOKEN);// لا تغير فيها شيء
-client2.login(process.env.TOKEN2);// لا تغير فيها شيء
+client.login("NDYyNDM0NjAzODk1MDk1Mjk3.DtCtDw.fOQjdUmhtm5-ShsLP6r6Xm_L83k");// لا تغير فيها شيء
+client2.login("MzQ4OTUzMTQwMzE1MjkxNjQ5.Dp998A.6tT2KKXRZ3EItpO9DRafk1g56Qc");// لا تغير فيها شيء
